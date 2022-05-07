83b41cd0cced615032dcea043d1c8ca686ee433d
AntonioGRZ <Antonio.gonzalez.rz@outlook.com>
2022-05-06 22:54:19 -0500

    docs agregado

---
+++ docs/precache-manifest.4699d7139daa227550e65510873ee6fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3c71d3b8cc37d0c9da4d303ada27011",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e5629cb43d4706992d4",
+    "url": "/static/css/main.f423f9c0.chunk.css"
+  },
+  {
+    "revision": "2aaae38fbcb0ad5c6be9",
+    "url": "/static/js/2.e1449281.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.e1449281.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e5629cb43d4706992d4",
+    "url": "/static/js/main.84284f5d.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
