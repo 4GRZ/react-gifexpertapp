183a28abfde7bae18be5dcb6661328a1295ed197
AntonioGRZ <Antonio.gonzalez.rz@outlook.com>
2022-05-06 23:41:30 -0500

    actualizado

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -2,7 +2,7 @@
 
 export const getGifts = async( category ) => {
 
-    const url = `http://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=WNpimpZwSDGTilnYTqXlpoCtxmOLZbNx`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=WNpimpZwSDGTilnYTqXlpoCtxmOLZbNx`;
     const resp = await fetch( url );
     const {data} = await resp.json();
     const gifs = data.map( img => {
